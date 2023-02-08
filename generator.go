f042e6f5794f268f17222b090dffde41e142abcc
chrnorm <17420369+chrnorm@users.noreply.github.com>
2023-02-08 13:42:44 +0000

    add generator

---
+++ generator.go
@@ -0,0 +1,97 @@
+package awsconfigfile
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"regexp"
+	"strings"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+	"gopkg.in/ini.v1"
+)
+
+type Source interface {
+	GetProfiles(ctx context.Context) ([]SSOProfile, error)
+}
+
+// Generator generates AWS profiles for ~/.aws/config.
+// It reads profiles from sources and merges them with
+// an existing ini config file.
+type Generator struct {
+	// Output to write to
+	Output              io.Writer
+	Sources             []Source
+	Config              *ini.File
+	NoCredentialProcess bool
+	ProfileNameTemplate string
+	Prefix              string
+}
+
+const profileSectionIllegalChars = ` \][;'"`
+
+// regular expression that matches on the characters \][;'" including whitespace, but does not match anything between {{ }} so it does not check inside go templates
+// this regex is used as a basic safeguard to help users avoid mistakes in their templates
+// for example "{{ .AccountName }} {{ .RoleName }}" this is invalid because it has a whitespace separating the template elements
+var profileSectionIllegalCharsRegex = regexp.MustCompile(`(?s)((?:^|[^\{])[\s\][;'"]|[\][;'"][\s]*(?:$|[^\}]))`)
+var matchGoTemplateSection = regexp.MustCompile(`\{\{[\s\S]*?\}\}`)
+
+var DefaultProfileNameTemplate = "{{ .AccountName }}/{{ .RoleName }}"
+
+// Generate AWS profiles and merge them with the existing config.
+// Writes output to the generator's output.
+func (g *Generator) Generate(ctx context.Context) error {
+	var eg errgroup.Group
+	var mu sync.Mutex
+	var profiles []SSOProfile
+
+	if strings.ContainsAny(g.Prefix, profileSectionIllegalChars) {
+		return fmt.Errorf("profile prefix must not contain any of these illegal characters (%s)", profileSectionIllegalChars)
+	}
+
+	// use the default template if it's not provided
+	if g.ProfileNameTemplate == "" {
+		g.ProfileNameTemplate = DefaultProfileNameTemplate
+	}
+
+	// check the profile template for any invalid section name characters
+	if g.ProfileNameTemplate != DefaultProfileNameTemplate {
+		cleaned := matchGoTemplateSection.ReplaceAllString(g.ProfileNameTemplate, "")
+		if profileSectionIllegalCharsRegex.MatchString(cleaned) {
+			return fmt.Errorf("profile template must not contain any of these illegal characters (%s)", profileSectionIllegalChars)
+		}
+	}
+
+	for _, s := range g.Sources {
+		scopy := s
+		eg.Go(func() error {
+			got, err := scopy.GetProfiles(ctx)
+			if err != nil {
+				return err
+			}
+			mu.Lock()
+			defer mu.Unlock()
+			profiles = append(profiles, got...)
+			return nil
+		})
+	}
+
+	err := eg.Wait()
+	if err != nil {
+		return err
+	}
+
+	err = Merge(MergeOpts{
+		Config:              g.Config,
+		SectionNameTemplate: g.ProfileNameTemplate,
+		Profiles:            profiles,
+		NoCredentialProcess: g.NoCredentialProcess,
+		Prefix:              g.Prefix,
+	})
+	if err != nil {
+		return err
+	}
+	_, err = g.Config.WriteTo(g.Output)
+	return err
+}
--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.1
+	golang.org/x/sync v0.1.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
